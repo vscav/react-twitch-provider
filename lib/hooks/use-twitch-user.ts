bb587a587ee46a864c373afffbcd9f1b74e4ff7d
vscav <vscavinner@gmail.com>
2022-10-04 14:24:56 +0200

    doc: add jsdoc for Twitch user data

--- lib/hooks/use-twitch-user.ts
+++ lib/hooks/use-twitch-user.ts
@@ -6,16 +6,59 @@ import { z } from 'zod'
 const USERS_API_ENDPOINT = 'users'
 
 const User = z.object({
+  /**
+   * User’s ID.
+   */
   id: z.string(),
+
+  /**
+   * User’s login name.
+   */
   login: z.string(),
+
+  /**
+   * User’s display name.
+   */
   display_name: z.string(),
+
+  /**
+   * User’s type: "staff", "admin", "global_mod", or "".
+   */
   type: z.string(),
+
+  /**
+   * The user’s broadcaster type: "partner", "affiliate", or "".
+   */
   broadcaster_type: z.string(),
+
+  /**
+   * User’s channel description.
+   */
   description: z.string(),
+
+  /**
+   * URL of the user’s profile image.
+   */
   profile_image_url: z.string(),
+
+  /**
+   * URL of the user’s offline image.
+   */
   offline_image_url: z.string(),
+
+  /**
+   * Total number of views of the user’s channel.
+   */
   view_count: z.number(),
+
+  /**
+   * User’s verified email address
+   */
   email: z.string(),
+
+  /**
+   * Date when the user was created.
+   */
   created_at: z.string(),
 })
 
