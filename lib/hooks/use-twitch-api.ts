826915ab8e09434f08cc93802bd2c8fd486b3f9c
vscav <vscavinner@gmail.com>
2022-10-28 00:13:23 +0200

    refactor: make API fetcher use http get method

--- lib/hooks/use-twitch-api.ts
+++ lib/hooks/use-twitch-api.ts
@@ -3,7 +3,6 @@ import useSWR from 'swr'
 import { TWITCH_API_BASE_URL } from '../constants/twitch-api'
 import { useTwitchContext } from '../context/use-twitch-context'
 import { FetcherError, generateError } from '../utils/error'
-import { HTTPMethod, HTTPMethods } from '../utils/http'
 
 type TwitchApiDataResponse<EntityType> = { data: EntityType }
 
@@ -14,27 +13,21 @@ type TwitchHookBaseReturn = {
 }
 type TwitchHookFetcherReturn<EntityDataType> = SWRResponse<EntityDataType, FetcherError>
 
-type CustomRequestInit = Pick<RequestInit, 'mode' | 'cache'>
+type CustomRequestInit = Pick<RequestInit, 'method' | 'mode' | 'cache'>
 const requestInit: CustomRequestInit = {
+  method: 'get',
   mode: 'cors',
   cache: 'no-store',
 }
 
-async function twitchApiFetcher<FetcherResponse>(
-  url: string,
-  method: HTTPMethod,
-  headers: HeadersInit,
-): Promise<FetcherResponse> {
-  const response = await fetch(url, { ...requestInit, method, headers })
+async function twitchApiFetcher<FetcherResponse>(url: string, headers: HeadersInit): Promise<FetcherResponse> {
+  const response = await fetch(url, { ...requestInit, headers })
   if (!response.ok) throw await generateError(response)
 
   return response.json()
 }
 
-function useTwitchApi<EntityDataType>(
-  endpoint: string,
-  method: HTTPMethod = HTTPMethods.get,
-): TwitchHookFetcherReturn<EntityDataType> {
+function useTwitchApi<EntityDataType>(endpoint: string): TwitchHookFetcherReturn<EntityDataType> {
   const { accessToken, clientId } = useTwitchContext()
 
   const path = `${TWITCH_API_BASE_URL}/${endpoint}`
@@ -43,7 +36,7 @@ function useTwitchApi<EntityDataType>(
     authorization: `Bearer ${accessToken}`,
   }
 
-  const fetcher: Fetcher<EntityDataType, string> = () => twitchApiFetcher<EntityDataType>(path, method, headers)
+  const fetcher: Fetcher<EntityDataType, string> = () => twitchApiFetcher<EntityDataType>(path, headers)
 
   return useSWR<EntityDataType, FetcherError>(path, fetcher, { shouldRetryOnError: false, refreshInterval: 10000 })
 }
